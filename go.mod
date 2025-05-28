2d4585c7fa268b3d4c4c07b1a2d3dc6e96754748
gainax2k1 <gainax2k1@gmail.com>
2025-05-27 19:13:58 -0700

    -Initial server creation for lesson 4

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gainax2k1/chirpy
+
+go 1.24.3
--- main.go
+++ main.go
@@ -1,1 +1,18 @@
-//  main to be here
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	newMux := http.NewServeMux()
+	newServer := http.Server{
+		Addr:    ":8080",
+		Handler: newMux,
+	}
+	err := newServer.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
