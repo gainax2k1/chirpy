e4aa007d4883db1e9723c49cad572ff2129a3032
gainax2k1 <gainax2k1@gmail.com>
2025-07-05 02:13:42 -0700

    added JWT

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -2,7 +2,10 @@ package auth
 
 import (
 	"fmt"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5" // go get -u github.com/golang-jwt/jwt/v5
+	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt" // "go get golang.org/x/crypto/bcrypt" //to install
 )
 
@@ -26,4 +29,54 @@ func CheckPasswordHash(password, hash string) error {
 		return fmt.Errorf("password does not match: %w", err)
 	}
 	return nil
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	var newClaims jwt.RegisteredClaims
+	newClaims.Issuer = "chirpy"
+
+	var timeNow jwt.NumericDate
+	timeNow.Time = time.Now().UTC()
+	newClaims.IssuedAt = &timeNow
+
+	var expireTime jwt.NumericDate
+	expireTime.Time = time.Now().Add(expiresIn).UTC()
+	newClaims.ExpiresAt = &expireTime
+
+	newClaims.Subject = userID.String()
+
+	newToken := jwt.NewWithClaims(jwt.SigningMethodHS256, newClaims)
+	jwtString, err := newToken.SignedString(tokenSecret)
+	if err != nil {
+		return "", fmt.Errorf("error signing token: %w", err)
+	}
+	return jwtString, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+
+	var registeredClaims jwt.RegisteredClaims
+
+	_, err := jwt.ParseWithClaims(tokenString, &registeredClaims,
+		func(token *jwt.Token) (interface{}, error) {
+			if token.Method != jwt.SigningMethodHS256 {
+				return nil, fmt.Errorf("wrong jwt signature")
+			}
+			return []byte(tokenSecret), nil
+		})
+
+	if err != nil {
+		return uuid.UUID{}, fmt.Errorf("error validating: %w", err)
+	}
+
+	userUUIDString, err := registeredClaims.GetSubject()
+	if err != nil {
+		return uuid.UUID{}, fmt.Errorf("error getting userUUID: %w", err)
+	}
+
+	userUUIDUUID, err := uuid.Parse(userUUIDString)
+	if err != nil {
+		return uuid.UUID{}, fmt.Errorf("error parsing userUUID: %w", err)
+	}
+	return userUUIDUUID, nil
 }
